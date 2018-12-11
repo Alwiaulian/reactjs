3d70b00f0ca08970c839c37deaf70d88c034a91f
Alwi Aulia Nuryadin <alwi.nuryadin@gmail.com>
2018-12-11 14:30:45 +0700

    first commit

---
+++ src/al.js
@@ -0,0 +1,13 @@
+import React, {component} from 'react';
+import logo from './logo.svg';
+import'./al.css';
+
+class Al extends Component{
+    render(){
+     return (
+        <div className="aku">
+
+        </div>
+     ) ;
+    }
+}
